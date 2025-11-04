e3570102d2e253e1f36dac146c6d8d8535220785
saravanansubramaniam <saravanan08071979@gmail.com>
2025-11-05 10:23:09 +1100

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-4xl">Texas AGI Labs</h1>
+    </div>
+  );
+}
