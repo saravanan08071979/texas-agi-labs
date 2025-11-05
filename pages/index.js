e6f67b790eb10cc884bda90ed7db2a378e6deefc
saravanansubramaniam <saravanan08071979@gmail.com>
2025-11-05 11:31:01 +1100

    deleted old structure

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-black text-white flex items-center justify-center">
-      <h1 className="text-4xl">Texas AGI Labs</h1>
-    </div>
-  );
-}
